f0b8cd8ffa947e2613519e0d6503338a399fdc2b
SageshiSai <saikonogameryt@gmail.com>
2023-02-23 13:13:05 +0100

    first commit

---
+++ src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import Controlador.Controlador;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Controlador controlador = new Controlador();
+    }
+}
